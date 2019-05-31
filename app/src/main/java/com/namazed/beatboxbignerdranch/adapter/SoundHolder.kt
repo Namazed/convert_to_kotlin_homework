da4641402f6eb91b1b41c06571f84acb57cdfa94
Igorlo <9440533@gmail.com>
2019-05-31 23:39:19 +0300

    hw

---
+++ app/src/main/java/com/namazed/beatboxbignerdranch/adapter/SoundHolder.kt
@@ -0,0 +1,34 @@
+@file: JvmName("SoundAdapter")
+
+package com.namazed.beatboxbignerdranch.adapter
+
+import android.view.View
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.namazed.beatboxbignerdranch.BeatBox
+import com.namazed.beatboxbignerdranch.Sound
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.list_item_sound.*
+
+class SoundHolder(override val containerView: View, private val beatBox: BeatBox) :
+    RecyclerView.ViewHolder(containerView),
+    View.OnClickListener, LayoutContainer {
+
+    private val soundButton: Button = list_item_sound_button
+    private var sound: Sound? = null
+
+    init {
+        soundButton.setOnClickListener(this)
+    }
+
+    fun bindSound(sound: Sound) {
+        this.sound = sound
+        soundButton.text = this.sound?.name
+    }
+
+    override fun onClick(v: View?) {
+        sound?.let {
+            beatBox.play(it)
+        }
+    }
+}

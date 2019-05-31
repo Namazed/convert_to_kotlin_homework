f5290e5a22f3a1a7e571a1df93d515be549a7a95
Alex <korneev.ry@yandex.ru>
2019-05-31 23:48:16 +0300

    converted

---
+++ app/src/main/java/com/namazed/beatboxbignerdranch/BeatBoxActivity.kt
@@ -0,0 +1,24 @@
+@file: JvmName("MyBeatBoxActivity")
+
+package com.namazed.beatboxbignerdranch
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+
+private const val LAYOUT: Int = R.layout.activity_fragment
+private const val CONTAINER_FRAGMENT: Int = R.id.fragment_container
+
+class BeatBoxActivity : AppCompatActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(LAYOUT)
+
+        val manager: FragmentManager = supportFragmentManager
+        manager.findFragmentById(CONTAINER_FRAGMENT) ?: manager.beginTransaction().run {
+            add(CONTAINER_FRAGMENT, BeatBoxFragment.newInstance())
+            commit()
+        }
+    }
+}
---
+++ app/src/main/java/com/namazed/beatboxbignerdranch/adapter/SoundHolder.kt
@@ -0,0 +1,32 @@
+@file: JvmName("SoundHolder")
+
+package com.namazed.beatboxbignerdranch.adapter
+
+import android.view.View
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.namazed.beatboxbignerdranch.BeatBox
+import com.namazed.beatboxbignerdranch.Sound
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.list_item_sound.*
+
+class SoundHolder(override val containerView: View, private val beatBox: BeatBox) :
+    RecyclerView.ViewHolder(containerView), LayoutContainer, View.OnClickListener {
+    private val soundButton: Button = list_item_sound_button
+    private var sound: Sound? = null
+
+    init {
+        soundButton.setOnClickListener(this)
+    }
+
+    internal fun bindSound(sound: Sound) {
+        this.sound = sound
+        soundButton.text = this.sound?.name
+    }
+
+    override fun onClick(view: View) {
+        sound?.let {
+            beatBox.play(it)
+        }
+    }
+}
